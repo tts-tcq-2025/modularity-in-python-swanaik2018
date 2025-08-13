3a186338a14f94e9944c56b211986db4b9c19d7b
swanaik2018 <swanaik2018@gmail.com>
2025-08-14 01:56:01 +0530

    Create reference_manual.py

---
+++ reference_manual.py
@@ -0,0 +1,13 @@
+from color_utils import get_color_from_pair_number
+
+def generate_reference_manual():
+    reference_lines = []
+    for pair_number in range(1, 26):  # 25 pairs
+        major, minor = get_color_from_pair_number(pair_number)
+        reference_lines.append(f"{pair_number:2} -> {major:<6} | {minor}")
+    return "\n".join(reference_lines)
+
+def print_reference_manual():
+    print("Color Code Reference Manual:")
+    print("-" * 32)
+    print(generate_reference_manual())
